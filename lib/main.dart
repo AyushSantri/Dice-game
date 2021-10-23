3d108b57b283066f72cd059d4d051bb1dfe20e2f
AyushSantri <ayushsantri222@gmail.com>
2021-10-23 19:23:50 +0530

    Learned and used "Expanded" widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,46 @@ import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
-        body: DicePage(),
-      ),
-    ),
+    DicePage(),
   );
 }
 
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Text('Dice'),
+          backgroundColor: Colors.red,
+        ),
+        body: Row(
+          children: [
+            Expanded(
+              child: Image.asset('images/dice1.png'),
+            ),
+            Expanded(
+              child: Image.asset('images/dice1.png'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
+// class DicePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.red,
+//         appBar: AppBar(
+//           title: const Text('Dice'),
+//           backgroundColor: Colors.red,
+//         ),
+//       ),
+//     );
+//   }
+// }
