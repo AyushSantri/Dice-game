748922c5080a49eec206aefdd8ac5411ae45f0a5
AyushSantri <ayushsantri222@gmail.com>
2021-10-23 19:39:24 +0530

    centered the dice and added padding

--- lib/main.dart
+++ lib/main.dart
@@ -16,32 +16,25 @@ class DicePage extends StatelessWidget {
           title: const Text('Dice'),
           backgroundColor: Colors.red,
         ),
-        body: Row(
-          children: [
-            Expanded(
-              child: Image.asset('images/dice1.png'),
-            ),
-            Expanded(
-              child: Image.asset('images/dice1.png'),
-            ),
-          ],
+        body: Center(
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Image.asset('images/dice1.png'),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Image.asset('images/dice1.png'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-// class DicePage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         backgroundColor: Colors.red,
-//         appBar: AppBar(
-//           title: const Text('Dice'),
-//           backgroundColor: Colors.red,
-//         ),
-//       ),
-//     );
-//   }
-// }
