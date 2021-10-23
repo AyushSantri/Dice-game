6e20f0290441ab886623fa5e1c4602f4b3c038ef
AyushSantri <ayushsantri222@gmail.com>
2021-10-23 20:34:02 +0530

    created a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,14 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,13 +28,17 @@ class DicePage extends StatelessWidget {
             children: [
               Expanded(
                 child: TextButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    print('Left button got pressed');
+                  },
                   child: Image.asset('images/dice1.png'),
                 ),
               ),
               Expanded(
                 child: TextButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    print('Right button got pressed');
+                  },
                   child: Image.asset('images/dice1.png'),
                 ),
               ),
