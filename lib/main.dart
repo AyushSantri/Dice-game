31b3ca4cc9e4022eda10986e49bdb2b85a1d2cb5
AyushSantri <ayushsantri222@gmail.com>
2021-10-23 19:55:18 +0530

    Added TextButton

--- lib/main.dart
+++ lib/main.dart
@@ -20,14 +20,14 @@ class DicePage extends StatelessWidget {
           child: Row(
             children: [
               Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
+                child: TextButton(
+                  onPressed: () {},
                   child: Image.asset('images/dice1.png'),
                 ),
               ),
               Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
+                child: TextButton(
+                  onPressed: () {},
                   child: Image.asset('images/dice1.png'),
                 ),
               ),
